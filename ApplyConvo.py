ccdc8105790c733ad07fb0250073cdf9e4767e4b
Joel TSHIAMALA KANYINDA, MSc candidate <96381991+tshiamalakanyindajoel29@users.noreply.github.com>
2025-10-25 17:18:08 +0200

    Update ApplyConvo.py

--- ApplyConvo.py
+++ ApplyConvo.py
@@ -1,18 +1,15 @@
 import streamlit as st
-import sys
-sys.path.append(convolution)
-from convolution import apply_convolution 
-
+from convolution import apply_convolution
 st.set_page_config(page_title="Convolution App", layout="centered")
 st.title("Application de Convolution avec TensorFlow")
 
 if st.button("Appliquer la convolution sur l'image"):
-    st.info("Exécution du script convolution.py ...")
+    st.info("Exécution du script `convolution.py`...")
     try:
         apply_convolution()
-        st.success("Convolution terminée. Résultat affiché dans une fenêtre externe.")
+        st.success("Convolution terminée ! Résultat affiché dans une fenêtre externe.")
     except Exception as e:
         st.error(f"Une erreur est survenue : {e}")
 
 st.markdown("---")
-st.caption("Assurez-vous que le fichier convolution.py contient bien la fonction apply_convolution() et que le chemin de l’image est correct.")
+st.caption("Assurez-vous que `convolution.py` contient la fonction `apply_convolution()` et que le chemin de l’image est correct.")
